5ab1c9e9a17714e36554a782ce71e8c36f60e7fa
xuxy <xuxy@fengyuntec.com>
2019-03-22 17:41:13 +0800

    add 数据库实体类

---
+++ godGameServer/Repository/Entity/GameAcctEntity.cs
@@ -0,0 +1,22 @@
+﻿
+
+using System;
+
+namespace godGameServer.Repository.Entity
+{
+    public class GameAcctEntity
+    {
+        public string id;
+        public string loginName;
+        public string tel;
+        public string email;
+        public string password;
+        public string loginIp;
+        public string instanceCode;
+        public string tenantCode;
+        public DateTime createDate;
+        public DateTime updateDate;
+        public string isDeleted;
+        public int version;
+    }
+}
